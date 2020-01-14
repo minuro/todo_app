e3c8b5b5ac0c50f11b89ee778216d44b073efec9
minuro <kotoribuki@gmail.com>
2020-01-15 01:08:56 +0900

    [add, update] RailsのAPI機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  namespace :api, format: 'json' do
+    resources :tasks, only: [:index, :create, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 end
